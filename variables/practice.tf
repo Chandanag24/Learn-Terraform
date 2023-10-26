7512dfeea3e95674751bb78bed388443ad645564
chandana24 <gantachandana98@gmail.com>
2023-10-26 21:04:36 +0530

    Terraform

--- variables/practice.tf
+++ variables/practice.tf
@@ -5,3 +5,37 @@ variable "emp_name" {
 output "emp_name" {
   value = var.emp_name
 }
+#List
+variable "emp_details" {
+  default = [
+   "chandana",
+    "chandu"
+  ]
+}
+
+#Map variable
+variable "emp_performance" {
+  default = {
+    chandana = 100
+    chandrika = 90
+  }
+}
+
+#map of map
+variable "emp_id_with_performance" {
+  default = {
+    id_s = {
+      c_id=48
+      ch_id=78
+    }
+    performance={
+      chandana=100
+      chandrika=90
+    }
+
+  }
+}
+
+output "emp_details" {
+  value = var.emp_details
+}

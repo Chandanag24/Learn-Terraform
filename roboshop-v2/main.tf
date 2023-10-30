3319cc652344f4517e984c556d946fa82124a8b9
chandana24 <gantachandana98@gmail.com>
2023-10-30 18:43:05 +0530

    Terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = lookup(each.value, null)
+    Name = lookup(each.value, "name", null)
   }
 }
 

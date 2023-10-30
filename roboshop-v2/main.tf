901c322284db27f3b21b0ea8283f2abab3bd9a1c
chandana24 <gantachandana98@gmail.com>
2023-10-30 18:34:09 +0530

    Terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -14,180 +14,42 @@ variable "zone_id" {
   default = "Z0243240SL2ZOPCKERO1"
 }
 
-resource "aws_instance" "frontend" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-
-  tags = {
-    Name = "frontend"
-  }
-}
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "frontend-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "mongodb"
-  }
-}
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "mongodb-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "catalogue"
-  }
-}
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "catalogue-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-resource "aws_instance" "redis" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "redis"
-  }
-}
-resource "aws_route53_record" "redis" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "redis-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-resource "aws_instance" "user" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "user"
-  }
-}
-resource "aws_route53_record" "user" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "user-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-resource "aws_instance" "cart" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "cart"
-  }
-}
-resource "aws_route53_record" "cart" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "cart-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "mysql"
-  }
-}
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "mysql-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-resource "aws_instance" "shipping" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "shipping"
-  }
-}
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "shipping-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "rabbitmq-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-resource "aws_instance" "payment" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "payment"
-  }
-}
-resource "aws_route53_record" "payment" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "payment-dev.chandana24.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
-
-resource "aws_instance" "dispatch" {
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-  tags = {
-    Name = "dispatch"
+variable "components" {
+  default = {
+    frontend = { name = "frontend-dev" }
+    mongodb = { name = "mongodb-dev" }
+    catalogue = { name = "catalogue-dev" }
+    redis = { name = "redis-dev" }
+    user = { name = "user-dev" }
+    cart = { name = "cart-dev" }
+    mysql = { name = "mysql-dev" }
+    shipping = { name = "shipping-dev" }
+    rabbitmq = { name = "rabbitmq-dev" }
+    payment = { name = "payment-dev" }
+    dispatch = { name = "dispatch-dev" }
   }
 }
 
-resource "aws_route53_record" "dispatch" {
-  zone_id = "Z0243240SL2ZOPCKERO1"
-  name    = "dispatch-dev.chandana24.online"
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
+
+  tags = {
+    Name = lookup(each.value, null)
+  }
+}
+
+resource "aws_route53_record" "record" {
+  for_each = var.components
+  zone_id = var.zone_id
+  name    = "${lookup(each.value, "name", null)}.chandana24.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.dispatch.private_ip]
+  records = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
+}
+
+output "instances" {
+  value= aws_instance.instance
 }

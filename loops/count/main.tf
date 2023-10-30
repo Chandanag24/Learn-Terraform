2ba4e5dac3720f13a7a6ec963300d78387d5304a
chandana24 <gantachandana98@gmail.com>
2023-10-30 16:01:24 +0530

    Terraform

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -2,14 +2,19 @@ variable "components" {
   default = ["frontend", "mongodb"]
 }
 
-resource "aws_instance" "instance" {
+#resource "aws_instance" "instance" {
+#  count= length(var.components)
+#
+#  ami           = "ami-081609eef2e3cc958"
+#  instance_type = "t3.small"
+#  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
+#
+#  tags = {
+#    Name = element(var.components,count.index)
+#  }
+#}
+
+resource "aws_security_group" "allow_tls" {
   count= length(var.components)
-
-  ami           = "ami-081609eef2e3cc958"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-02b5746c2dc0c1fad"]
-
-  tags = {
-    Name = element(var.components,count.index)
-  }
+  name = element(var.components,count.index)
 }
